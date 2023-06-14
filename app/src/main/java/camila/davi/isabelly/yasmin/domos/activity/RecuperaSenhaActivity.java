0327581183bd22b5b6e6857a3aaa338f9c654d5e
Camila Egydio <91480234+cfegydio@users.noreply.github.com>
2023-06-14 08:38:12 -0300

    1406

--- app/src/main/java/camila/davi/isabelly/yasmin/domos/activity/RecuperaSenhaActivity.java
+++ app/src/main/java/camila/davi/isabelly/yasmin/domos/activity/RecuperaSenhaActivity.java
@@ -1,16 +1,17 @@
 package camila.davi.isabelly.yasmin.domos.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import camila.davi.isabelly.yasmin.domos.R;
 
-public class RecuperarSenhaActivity extends AppCompatActivity {
+public class RecuperaSenhaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recuperar_senha);
+        setContentView(R.layout.activity_recupera_senha);
     }
+
 }

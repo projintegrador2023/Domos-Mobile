e7608f22faacf09a37812938edff6d90a7a602da
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-06-14 08:39:24 -0300

    imagens consertadas e splash screen

--- app/src/main/java/camila/davi/isabelly/yasmin/domos/activity/MainActivity.java
+++ app/src/main/java/camila/davi/isabelly/yasmin/domos/activity/MainActivity.java
@@ -2,7 +2,9 @@ package camila.davi.isabelly.yasmin.domos.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import camila.davi.isabelly.yasmin.domos.R;
 
@@ -12,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarLogin();
+            }
+        }, 2000);
+    }
+
+    private void mostrarLogin(){
+        Intent i = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(i);
     }
 }

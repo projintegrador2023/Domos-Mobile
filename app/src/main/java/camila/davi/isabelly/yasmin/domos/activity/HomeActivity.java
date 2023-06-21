b3d2fa70d26a1a7f80d8d91149966d642f5da347
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-06-21 08:46:01 -0300

    tela avisos quase pronta

--- app/src/main/java/camila/davi/isabelly/yasmin/domos/activity/HomeActivity.java
+++ app/src/main/java/camila/davi/isabelly/yasmin/domos/activity/HomeActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import camila.davi.isabelly.yasmin.domos.R;
 
-public class AvisosActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_avisos);
+        setContentView(R.layout.activity_home);
     }
 }

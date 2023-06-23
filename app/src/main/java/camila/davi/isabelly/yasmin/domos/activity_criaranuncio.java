40b4d270fb1036ec71697c28748700d368f0de6b
Camila Egydio <91480234+cfegydio@users.noreply.github.com>
2023-06-23 09:14:39 -0300

    Tudo ok

---
+++ app/src/main/java/camila/davi/isabelly/yasmin/domos/activity_criaranuncio.java
@@ -0,0 +1,14 @@
+package camila.davi.isabelly.yasmin.domos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_criaranuncio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criaranuncio);
+    }
+}

195740a819ca4dd246e25202347de50c57baa995
Camila Egydio <91480234+cfegydio@users.noreply.github.com>
2023-09-19 08:21:57 -0300

    1909 cedo

---
+++ app/src/main/java/camila/davi/isabelly/yasmin/domos/activity/EditarAnuncioActivity.java
@@ -0,0 +1,17 @@
+package camila.davi.isabelly.yasmin.domos.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import camila.davi.isabelly.yasmin.domos.R;
+
+public class EditarAnuncioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_anuncio);
+    }
+}
